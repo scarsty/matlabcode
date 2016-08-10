55bbbb1d9fbe3a459f4682a8067a095055d70d9d
scarsty <scarsty@gmail.com>
2016-08-10 23:46:50 +0800

    stretch函数未完成

--- draw/draw_fluw_field2.m
+++ draw/draw_fluw_field2.m
@@ -4,25 +4,24 @@ B=A(l/2+1:l,2:5);
 quiver(B(:,2),B(:,1),B(:,4),B(:,3))
 axis equal tight
 
-for i=1:l/2
-    x=B(i,1);
-    y=B(i,2);
-    u=B(i,3);
-    v=B(i,4);
-    U(x+1,y+1)=u;
-    V(x+1,y+1)=v;
-    
-end
+% for i=1:l/2
+%     x=B(i,1);
+%     y=B(i,2);
+%     u=B(i,3);
+%     v=B(i,4);
+%     U(x+1,y+1)=u;
+%     V(x+1,y+1)=v;    
+% end
+
+%streamslice(U,V);
+%axis equal tight
 
 k=0;
-s=zeros(79,1);
+s=zeros(80,1);
 for i=1:1185
     a = B(i,2)+1;
     if ~isnan(B(i,4))
-        s(a,1)=s(a,1)+B(i,4)*abs(B(i,1)-8);
+        s(a,1)=s(a,1)+B(i,4);%*abs(B(i,1)-8);
     end
 end
-
 %plot(s);
-%streamslice(U,V);
-%axis equal tight
--- stretch.m
+++ stretch.m
@@ -1,28 +1,36 @@
-t1=double(imread('t1.bmp'))/255;
-t0=double(imread('t0.bmp'))/255;
-t1d=double(imread('t1d.bmp'))/255;
-tic
-%X=zeros(120000, 8000);
-b=rand(120000,1);
-toc
-ii=zeros(400*300*20,1);
-jj=zeros(400*300*20,1);
-ss=zeros(400*300*20,1);
-c=1;
-for i=1:400
-    for j=1:300
-    for k=1:20
-        %X(i+(j-1)*400,i+(k-1)*400)=rand();
-        ii(c)=i+(j-1)*400;
-        jj(c)=i+(k-1)*400;
-        ss(c)=rand();
-        c=c+1;
-    end
+%t1=double(imread('t1.bmp'))/255;
+
+t0=double(imread('t0.bmp'))/255;%standard image
+t1d=double(imread('t1d.bmp'))/255;%deformed image with other info
+
+%find
+diffm = 20;
+[m0,n0] = size(t0);
+M_t0 = m0*n0;
+%transfer t0 to vector b
+b=t0(:);
+
+[m1,n1] = size(t1d);
+N = m1*n1*(diffm*2+1);
+ii=zeros(N,1);
+jj=zeros(N,1);
+ss=zeros(N,1);
+count=0;
+for i=1:m1
+    for j=1:n1
+        for k=-diffm:diffm
+            id=i+k;
+            if id>=1 && id<=m1
+                count=count+1;
+                %todo: unfinished, seems not right
+                ii(count)=i+(j-1)*m1;
+                jj(count)=i+(-1)*400;
+                ss(count)=t1d(id,j);
+            end
+        end
     end
 end
-toc
-%X_SP=sparse(X);
-X_SP=sparse(ii,jj,ss,120000,8000);
-toc
-a=X_SP\b;
-toc
+
+X_SP=sparse(ii(1:count),jj(1:count),ss(1:count),m1*n1,count);
+% toc
+% a=X_SP\b;
