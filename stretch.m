6a25c51000b89f8c26da111b52a9b7efcede4ca7
scarsty <scarsty@gmail.com>
2016-08-11 10:17:10 +0800

    no message

--- stretch.m
+++ stretch.m
@@ -4,33 +4,46 @@ t0=double(imread('t0.bmp'))/255;%standard image
 t1d=double(imread('t1d.bmp'))/255;%deformed image with other info
 
 %find
-diffm = 20;
+diffm = 10;
 [m0,n0] = size(t0);
 M_t0 = m0*n0;
 %transfer t0 to vector b
 b=t0(:);
-
+tic
 [m1,n1] = size(t1d);
+n=n0;
 N = m1*n1*(diffm*2+1);
 ii=zeros(N,1);
 jj=zeros(N,1);
 ss=zeros(N,1);
 count=0;
-for i=1:m1
-    for j=1:n1
+for i0=1:m0
+    i1 = int32(i0*m1/m0);
+    for j=1:n
         for k=-diffm:diffm
-            id=i+k;
+            id=i1+k;
             if id>=1 && id<=m1
                 count=count+1;
                 %todo: unfinished, seems not right
-                ii(count)=i+(j-1)*m1;
-                jj(count)=i+(-1)*400;
-                ss(count)=t1d(id,j);
+                ii(count)=i0+(j-1)*m0;
+                jj(count)=id+(j-1)*m1;
+                ss(count)=t1d(i0,j);
             end
         end
     end
 end
-
-X_SP=sparse(ii(1:count),jj(1:count),ss(1:count),m1*n1,count);
-% toc
-% a=X_SP\b;
+toc
+X_SP=sparse(ii(1:count),jj(1:count),ss(1:count),m0*n1,m0*m1);
+toc
+a=X_SP\b;
+toc
+A=zeros(m0,m1);
+for i0=1:m0
+    for i1=1:m1
+        %if abs(i1-i0)<=diffm
+            A(i0,i1)=a(i0+(i1-1)*m0);
+        %end
+    end
+end
+t0d=A*t1d;
+imshow(t0d);
